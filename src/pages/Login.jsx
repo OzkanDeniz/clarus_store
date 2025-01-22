18cc4bbf989f967ca699a9436f841f342f7a76ff
ozkan <ozkandnz09@gmail.com>
2025-01-22 23:08:57 +0300

    sign in

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,8 +6,13 @@ const Login = () => {
       <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col justify-center  text-center">
         <div className="flex justify-center items-center mt-2 gap-2">
           <span className="w-[6px] h-[39px] bg-yellow-500 "></span>
-          <h1 className="text-[22px] sm:text-[32px] font-montserrat font-[700] uppercase ">Clarus Store</h1>
-
+          <h1 className="text-[22px] sm:text-[32px] font-montserrat font-[700] uppercase ">
+            Clarus Store
+          </h1>
+        </div>
+        <div className="my-2">
+          <h3 className="font-montserrat font-[600] text-[22px]">SIGN IN</h3>
+          <p className="font-montserrat text-labelColor text-label mt-1">Enter your credentials to access your account</p>
         </div>
       </div>
     </div>
