6d67c4759720cf7b404a9b026b39af6a37f92d2c
ozkan <ozkandnz09@gmail.com>
2025-01-22 23:18:03 +0300

    label and input

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -14,6 +14,12 @@ const Login = () => {
           <h3 className="font-montserrat font-[600] text-[22px]">SIGN IN</h3>
           <p className="font-montserrat text-labelColor text-label mt-1">Enter your credentials to access your account</p>
         </div>
+        <form className="flex flex-col text-left p-3 gap-5">
+          <div className="flex flex-col gap-2">
+            <label className="font-montserrat text-label text-labelColor hover:after:content-['admin@aa.com'] hover:after:text-black hover:after:pl-3 hover:after:underline hover:cursor-pointer" htmlFor="email">Email</label>
+            <input type="email" />
+          </div>
+        </form>
       </div>
     </div>
   );
