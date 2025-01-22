17a0a99055648b05a7219d0aa2eb16ae4d68acd6
ozkan <ozkandnz09@gmail.com>
2025-01-22 23:02:35 +0300

    clarus store

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,9 +4,10 @@ const Login = () => {
   return (
     <div className="loginDiv">
       <div className="h-[500px] w-11/12 sm:w-[475px] bg-white rounded-[20px] p-5 flex flex-col justify-center  text-center">
-        <div>
-          <span></span>
-          <h1 className="text-[22px] sm:text-[32px]">Clarus Store</h1>
+        <div className="flex justify-center items-center mt-2 gap-2">
+          <span className="w-[6px] h-[39px] bg-yellow-500 "></span>
+          <h1 className="text-[22px] sm:text-[32px] font-montserrat font-[700] uppercase ">Clarus Store</h1>
+
         </div>
       </div>
     </div>
