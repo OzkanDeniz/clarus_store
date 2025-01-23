3e74b5a80e4af29da6eef69b53db743382d58e0d
ozkan <ozkandnz09@gmail.com>
2025-01-23 19:15:15 +0300

    login

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -5,8 +5,13 @@ export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+
+  const login =(info)=>{
+    setUser(info)
+
+  }
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, login }}>{children}</AuthContext.Provider>
   );
 };
 export default AuthProvider;
