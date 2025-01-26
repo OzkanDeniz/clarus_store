c695a8bbcd113dbf2aed745686d3cc46e0dee4b9
ozkan <ozkandnz09@gmail.com>
2025-01-26 14:16:55 +0300

    usenavigate

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,13 +1,16 @@
 import { createContext } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 // export const {Provider} = createContext()
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const navigate = useNavigate()
   console.log(user);
   const login = (info) => {
     setUser(info);
+    navigate("/dashboard")
   };
   return (
     <AuthContext.Provider value={{ user, login }}>
