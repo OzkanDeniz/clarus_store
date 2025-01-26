0e9798986425b71beae6d100b665abcf1c740705
ozkan <ozkandnz09@gmail.com>
2025-01-26 14:41:33 +0300

    logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { closeNavbar, openNavbar } from "../helper/icons";
+import { closeNavbar, logoutIcon, openNavbar } from "../helper/icons";
 import { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 const navigation = [
@@ -20,8 +20,8 @@ const navigation = [
 
 const Navbar = () => {
   const [show, setshow] = useState(false);
-  const location = useLocation()
-  console.log(location.pathname)
+  const location = useLocation();
+  console.log(location.pathname);
   return (
     <nav className="bg-navbarColor md:text-sm">
       <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
@@ -43,14 +43,33 @@ const Navbar = () => {
             </button>
           </div>
         </div>
-        <div className={`${show ? "flex flex-col pb-2 " : "hidden"} md:flex md:flex-row flex-1 items-center` }>
+        <div
+          className={`${
+            show ? "flex flex-col pb-2 " : "hidden"
+          } md:flex md:flex-row flex-1 items-center`}
+        >
           <ul className="md:flex md:space-x-6">
             {navigation.map((item) => (
-              <li className="text-gray-700 font-medium flex justify-center" key={item.title}>
-                <NavLink className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white ${location.pathname === item.path ? "underline scale-110" : ""}`} to={item.path}>{item.title}</NavLink>
+              <li
+                className="text-gray-700 font-medium flex justify-center"
+                key={item.title}
+              >
+                <NavLink
+                  className={`block hover:bg-main rounded-full py-2 px-4 hover:text-white ${
+                    location.pathname === item.path ? "underline scale-110" : ""
+                  }`}
+                  to={item.path}
+                >
+                  {item.title}
+                </NavLink>
               </li>
             ))}
           </ul>
+          <div className="flex-1 items-center justify-end gap-x-6 mt-6 space-y-6 md:flex md:space-y-0 md:mt-0">
+            <NavLink className="flex items-center justify-center gap-x-1 py-2 px-4 font-medium text-gray-700 hover:bg-main hover:text-white rounded-full md:inline-flex ">
+              Logout {logoutIcon}
+            </NavLink>
+          </div>
         </div>
       </div>
     </nav>
