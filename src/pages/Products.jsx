9c7cbf207491f56a7015b9e06e7f6ce1599a1791
ozkan <ozkandnz09@gmail.com>
2025-01-29 22:51:42 +0300

    search

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -26,7 +26,7 @@ const Products = () => {
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [search]);
 
   return (
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg-px-8">
