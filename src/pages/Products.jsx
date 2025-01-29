87a8fa1f1bb37fc0ad159f2f87cddee0a4ecf2c6
ozkan <ozkandnz09@gmail.com>
2025-01-29 16:53:09 +0300

    axios

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Products = () => {
+  const [products, setproducts] = useState([])
+
+  const getData= async()=>{
+try {
+  const res = await axios.get()
+} catch (error) {
+  
+}
+
+  }
   return (
     <div>Products</div>
   )
