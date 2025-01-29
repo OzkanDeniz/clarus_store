e81dd1d1cf604605d6026958d089065783cf876a
ozkan <ozkandnz09@gmail.com>
2025-01-29 16:33:29 +0300

    getitem

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,24 +1,25 @@
 import { createContext } from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+
 export const AuthContext = createContext();
 // export const {Provider} = createContext()
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const navigate = useNavigate()
+  const [user, setUser] = useState(JSON.parse(sessionStorage.getItem(user)) || null);
+  const navigate = useNavigate();
   console.log(user);
   const login = (info) => {
     setUser(info);
-    navigate("/dashboard")
+    navigate("/dashboard");
   };
 
-  const logout =()=>setUser(null)
+  const logout = () => setUser(null);
 
   useEffect(() => {
-   sessionStorage.setItem("user",JSON.stringify(user))
-  }, [user])
-  
+    sessionStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
   return (
     <AuthContext.Provider value={{ user, login, logout }}>
       {children}
