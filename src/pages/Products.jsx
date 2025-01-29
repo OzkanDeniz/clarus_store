8d4742eaef7afcd12775e9e56ea791e5e56d7f44
ozkan <ozkandnz09@gmail.com>
2025-01-29 22:36:35 +0300

    searchinput

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import ProductCard from "../components/ProductCard";
 import Loading from "../components/Loading";
+import SearchInput from "../components/SearchInput";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -29,6 +30,7 @@ const Products = () => {
 
   return (
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg-px-8">
+      <SearchInput/>
       <h2 className="text-2xl font-bold mt-8 tracking-tight text-grey-900">
         All Products
       </h2>
