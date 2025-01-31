6a3f7e276124d4ef05336408eab836369a2fed33
ozkan <ozkandnz09@gmail.com>
2025-02-01 02:06:32 +0300

    useProductsContext

--- src/context/ProductProvider.jsx
+++ src/context/ProductProvider.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useContext } from "react";
+import axios from "axios";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const ProductContext = createContext();
 
@@ -6,7 +7,29 @@ export const useProductsContext = () => {
   return useContext(ProductContext);
 };
 const ProductProvider = ({ children }) => {
-  const values = {};
+  const [products, setProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const getData = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(
+        `https://dummyjson.com/products/search?q=${search}`
+      );
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [search]);
+
+  const values = { products, loading, setSearch, search };
   return (
     <ProductContext.Provider value={values}>{children}</ProductContext.Provider>
   );
