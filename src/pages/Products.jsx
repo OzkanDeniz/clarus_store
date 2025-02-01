fa0660bc783a6e43e8dc769fa4cc5d2719a57a73
ozkan <ozkandnz09@gmail.com>
2025-02-01 15:39:08 +0300

    about

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -11,7 +11,7 @@ const Products = () => {
 
   return (
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg-px-8">
-      <SearchInput/>
+      <SearchInput />
       <h2 className="text-2xl font-bold mt-8 tracking-tight text-grey-900">
         All Products
       </h2>
